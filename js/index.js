a4837652871cfe0eb7a27b1277173230723b7655
ParkSangEun2016212014 <71581398+ParkSangEun2016212014@users.noreply.github.com>
2022-12-14 18:27:53 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,10 @@
+//dream6
+
+$(setTimeout(function () {
+    alert("전 꿈이 선물상자라고 생각해요.\n언제든지 열고 싶을 때 열 수 있는.")
+    alert("무엇이 들어있을지는 당신만이 알겠죠.")
+    alert("그게 무엇이던 간에,\n하나 확실한 건 당신이 좋아할 수 밖에 없을 거라는 거죠.")
+    alert("언제라도 준비가 되면 상자를 열어보세요. 항상 화이팅!")
+    alert("귀여운 고양이로 마무리하겠습니다.")
+    document.getElementById("background").src = "img/cat.png";
+}, 1000));
